60b360515a9dd9c31b374f8469930fb6c129d4dc
Yuhei Okazaki <okazaki@fusic.co.jp>
2021-09-07 09:28:14 +0900

    Initialize.

---
+++ lib/fusic/cli/ruby.rb
@@ -0,0 +1,10 @@
+require "fusic/cli/ruby/version"
+
+module Fusic
+  module Cli
+    module Ruby
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/fusic/cli/ruby/version.rb
@@ -0,0 +1,7 @@
+module Fusic
+  module Cli
+    module Ruby
+      VERSION = "0.1.0"
+    end
+  end
+end
